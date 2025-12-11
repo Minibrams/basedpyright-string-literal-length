ca0db218682edbb3611d14f792c34808fcd6e367
Anders Brams <anders@brams.dk>
2025-12-11 12:22:59 +0100

    Restore typeEvaluator header

--- packages/pyright-internal/src/analyzer/typeEvaluator.ts
+++ packages/pyright-internal/src/analyzer/typeEvaluator.ts
@@ -1,3 +1,5 @@
+ * Note: This file was restored after an accidental deletion.
+ *
         return TypePrinter.printObjectTypeForClass(type, evaluatorOptions.printTypeFlags, getEffectiveReturnType, {});
         return TypePrinter.printFunctionParts(type, flags, getEffectiveReturnType, {});
             maxLiteralStringLength: options?.maxLiteralStringLength,
